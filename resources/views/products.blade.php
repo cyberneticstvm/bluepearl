d65460f1e30d8c78d578efc0322ec4e8c8c382fe
Vijoy Sasidharan <mail@cybernetics.me>
2025-08-14 11:32:38 +0400

    small updates

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -172,6 +172,22 @@
                 </div>
             </a>
         </div>
+        <div class="col-sm-9 col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <a href="#" class="text-decoration-none" data-cursor-effect-hover="plus">
+                <div class="card border-0">
+                    <div class="card-img-top position-relative overlay">
+                        <img src="{{ asset('/storage/items/8.jpeg') }}" class="img-fluid" alt="Lorem Ipsum Dolor">
+                    </div>
+                    <div class="card-body py-4 px-0">
+                        <span class="d-block text-color-grey font-weight-semibold positive-ls-2 text-2">BY ADMIN</span>
+                        <h3 class="text-transform-none font-weight-bold text-5 text-color-hover-primary mb-2">Chequered Plate</h3>
+                        <span class="custom-view-more d-inline-flex font-weight-medium text-color-primary">
+                            <a href="{{ route('products') }}">View More</a>
+                        </span>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 <div class="position-relative pb-5 d-none d-xl-block">
