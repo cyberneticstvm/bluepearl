e2addc02a70550c1ca5668049a32a3bc24dc030c
Cybernetics <mail@cybernetics.me>
2025-12-06 10:12:20 +0530

    small updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -30,7 +30,7 @@
                     <a href="tel:+971569956740" class="d-flex align-items-center text-decoration-none text-color-dark text-color-hover-primary font-weight-bold text-3-5">
                         <img width="23" height="23" src="{{ asset('assets/img/demos/construction/icons/phone.svg') }}" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-dark me-2'}" />
                         +971 569956740
-                    </a> |
+                    </a>
                     <a href="tel:+971549905060" class="d-flex align-items-center text-decoration-none text-color-dark text-color-hover-primary font-weight-bold text-3-5">
                         <img width="23" height="23" src="{{ asset('assets/img/demos/construction/icons/phone.svg') }}" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-dark me-2'}" />
                         +971 549905060
